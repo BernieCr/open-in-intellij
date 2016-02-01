c4b102a93fc3aa0195a3143cd68bd12e66f5b0e5
Bernhard Caspar <hello@bernhardcaspar.de>
2016-02-01 13:34:53 +0100

    Use injected XHR (Electron fix)

--- src/openinide.js
+++ src/openinide.js
@@ -6,28 +6,82 @@
 
     var openInIde = function(ideUrl) {
         window.location.assign(ideUrl);
+        //chrome.devtools.inspectedWindow.eval('console.log("'+ideUrl+'")');
     };
 
-    var readURLContent = function(url, callback) {
+    // parseUri 1.2.2
+    // (c) Steven Levithan <stevenlevithan.com>
+    // MIT License
+    var parseUri = function(str) {
+        var o = {
+            key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+            q:   {
+                name:   "queryKey",
+                parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+            },
+            parser: {
+                strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/
+            }
+        };
+
+        var	m   = o.parser["strict"].exec(str),
+            uri = {},
+            i   = 14;
+
+        while (i--) uri[o.key[i]] = m[i] || "";
+
+        uri[o.q.name] = {};
+        uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+            if ($1) uri[o.q.name][$1] = $2;
+        });
+
+        return uri;
+    };
+
+    var readURLContent = function(url) {
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
             if (xhr.readyState == XMLHttpRequest.DONE) {
-                console.log(xhr.responseText);
-                callback(xhr.responseText);
+                window.OPENINIDE_APP_PATHS_RETURN = xhr.responseText;
+                //callback(xhr.responseText);
             }
         };
         xhr.open('GET', url, true);
         xhr.send(null);
     };
 
+
     chrome.devtools.panels.setOpenResourceHandler(function(resource, lineNumber) {
+
+        var iRetry = 0;
+
+        var checkUrlResponse = function() {
+            chrome.devtools.inspectedWindow.eval('(window.OPENINIDE_APP_PATHS_RETURN || "")', function (ret) {
+                if (ret) {
+                    var paths = JSON.parse(ret);
+                    ideUrls[thisUrl] = 'ide://'+paths.appFolder;
+
+                    openInIde(ideUrls[thisUrl]+fileString);
+
+                    chrome.devtools.inspectedWindow.eval('(delete window.OPENINIDE_APP_PATHS_RETURN)');
+                }
+                else {
+                    iRetry++;
+                    if (iRetry < 10) {
+                        setTimeout(checkUrlResponse, 250);
+                    }
+                    else {
+                        chrome.devtools.inspectedWindow.eval('console.error("OpenInIde: Couldn\'t get app paths from '+thisUrl+'");');
+                    }
+                }
+            });
+        };
+
+
         var url = resource.url;
-        console.log(url);
 
-        var parser = document.createElement('a');
-        parser.href = url;
-
-        var thisUrl = parser.protocol+'//'+parser.hostname+(parser.port ? ':'+parser.port: '');
+        var urlParse = parseUri(url);
+        var thisUrl = urlParse.protocol+'://'+urlParse.host+(urlParse.port ? ':'+urlParse.port: '');
 
         var regex = new RegExp(thisUrl, 'i');
         var fileString = url.replace(regex, '');
@@ -37,18 +91,15 @@
             openInIde(ideUrls[thisUrl]+fileString);
         }
         else {
-            console.log(thisUrl+'/get-app-paths');
-            readURLContent(thisUrl+'/get-app-paths', function(sPaths) {
-                var paths = JSON.parse(sPaths);
-                ideUrls[thisUrl] = 'ide://'+paths.appFolder+'/';
-                openInIde(ideUrls[thisUrl]+fileString);
-            });
+            chrome.devtools.inspectedWindow.eval('('+readURLContent.toString()+')("'+thisUrl+'/get-app-paths'+'");');
+            checkUrlResponse();
         }
 
 
     });
 
     // require('electron').remote.BrowserWindow.addDevToolsExtension('/Users/johnny/Coding/_Tools/chrome-openinide/src');
+    // require('electron').remote.BrowserWindow.removeDevToolsExtension('Open In IDE');
 
 
 })();
