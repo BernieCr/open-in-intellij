e3e409db622302e254a19aa6f340ab5cdffa1cb9
Bernhard Caspar <hello@bernhardcaspar.de>
2016-05-13 14:04:32 +0200

    Move to new IntelliJ REST API :) + rename project

--- src/openinide.js
+++ src/openinide.js
@@ -2,12 +2,20 @@
 
 (function() {
 
-    var ideUrls = {};
 
-    var openInIde = function(ideUrl) {
-        window.location.assign(ideUrl);
-        //chrome.devtools.inspectedWindow.eval('console.log("'+ideUrl+'")');
+    var rootPaths = {
+        'www.eventnoted.com': 'dist/'
+
     };
+
+    var devConsoleError = function(message) {
+        chrome.devtools.inspectedWindow.eval('console.error("'+message+'");');
+    };
+
+    var devConsoleLog = function(message) {
+        chrome.devtools.inspectedWindow.eval('console.log("'+message+'");');
+    };
+
 
     // parseUri 1.2.2
     // (c) Steven Levithan <stevenlevithan.com>
@@ -39,68 +47,73 @@
     };
 
     var readURLContent = function(url) {
+
         var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
+
+        var xhrEvent = function() {
             if (xhr.readyState == XMLHttpRequest.DONE) {
-                window.OPENINIDE_APP_PATHS_RETURN = xhr.responseText;
-                //callback(xhr.responseText);
+                switch (xhr.status) {
+                    case 200:
+                        // OK
+                        //devConsoleLog('Opening '+url);
+                        break;
+                    case 0:
+                        devConsoleError('Couldn\'t open file in IntelliJ! Please first start your IDE and open the project. (IntelliJ API Request URL: ' + url + ')');
+                        break;
+                    case 404:
+                        devConsoleError('Couldn\'t open file in IntelliJ! File not found. Please make sure the project is opened or specify root path. (IntelliJ API Request URL: ' + url + ')');
+                        break;
+                    default:
+                        devConsoleError('Couldn\'t open file in IntelliJ! HTTP error ' + xhr.status + ' for IntelliJ API Request URL ' + url);
+                        break;
+                }
+                xhr = null;
             }
         };
+
+        xhr.onreadystatechange = xhrEvent;
+        xhr.onerror = xhrEvent;
         xhr.open('GET', url, true);
         xhr.send(null);
     };
 
 
     chrome.devtools.panels.setOpenResourceHandler(function(resource, lineNumber) {
-
-        var iRetry = 0;
-
-        var checkUrlResponse = function() {
-            chrome.devtools.inspectedWindow.eval('(window.OPENINIDE_APP_PATHS_RETURN || "")', function (ret) {
-                if (ret) {
-                    var paths = JSON.parse(ret);
-                    ideUrls[thisUrl] = 'ide://'+paths.appFolder;
-
-                    openInIde(ideUrls[thisUrl]+fileString);
-
-                    chrome.devtools.inspectedWindow.eval('(delete window.OPENINIDE_APP_PATHS_RETURN)');
-                }
-                else {
-                    iRetry++;
-                    if (iRetry < 10) {
-                        setTimeout(checkUrlResponse, 125);
-                    }
-                    else {
-                        openInIde('ide://'+fileString);
-                        chrome.devtools.inspectedWindow.eval('console.error("OpenInIde: Couldn\'t get app paths from '+thisUrl+'");');
-                    }
-                }
-            });
-        };
-
-
+        
         var url = resource.url;
 
         var urlParse = parseUri(url);
-        var thisUrl = urlParse.protocol+'://'+urlParse.host+(urlParse.port ? ':'+urlParse.port: '');
+
+        var thisServer = urlParse.host+(urlParse.port ? ':'+urlParse.port: '');
+        var thisUrl = urlParse.protocol+'://'+thisServer;
 
         var regex = new RegExp(thisUrl, 'i');
-        var fileString = url.replace(regex, '');
-        fileString += ':'+lineNumber;
-
-        if (ideUrls[thisUrl]) {
-            openInIde(ideUrls[thisUrl]+fileString);
-        }
-        else {
-            chrome.devtools.inspectedWindow.eval('('+readURLContent.toString()+')("'+thisUrl+'/get-app-paths'+'");');
-            checkUrlResponse();
+        var fileString = url.replace(regex, ''); // http://xxx aus URL löschen => nur Dateipfad erhalten
+        if (lineNumber) {
+            fileString += ':' + lineNumber;
         }
 
+        if (fileString[0] == '/') {
+            fileString = fileString.substr(1);
+        }
 
+        // nachsehen, ob wir für den aktuellen Host einen Pfad hinterlegt haben und ggf. verwenden
+        if (rootPaths[thisServer]) {
+            fileString = rootPaths[thisServer]+fileString;
+        }
+
+        //
+        // IntelliJ REST API!
+        // http://develar.org/idea-rest-api/
+        //
+        var ideOpenUrl = 'http://localhost:63342/api/file/'+fileString;
+
+       // devConsoleLog(ideOpenUrl);
+
+        readURLContent(ideOpenUrl);
+        
     });
 
-    // require('electron').remote.BrowserWindow.addDevToolsExtension('/Users/johnny/Coding/_Tools/chrome-openinide/src');
-    // require('electron').remote.BrowserWindow.removeDevToolsExtension('Open In IDE');
 
 
 })();
